f8d8bed69c51acbd25cba01a4f49afed80b479e1
danijax <aliibra19@yahoo.com>
2016-12-08 09:28:29 +0100

    Add test for Movies Endpoint

---
+++ app/src/test/java/com/danieljames/popularmovies/data/MoviesEndPointTest.java
@@ -0,0 +1,30 @@
+package com.danieljames.popularmovies.data;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by danieljames on 12/8/16.
+ */
+public class MoviesEndPointTest {
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void serviceGetsListOfMoviesFromApi(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+
+    public void serviceGetsAMovieFromApi(){
+        fail("Not yet Implemented");
+    }
+
+}
